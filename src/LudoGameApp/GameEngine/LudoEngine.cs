04f8bd997d21254518ab27f8f8e7dee7a0e8a1e6
nhassiakis <40218955+nhassiakis@users.noreply.github.com>
2019-01-08 14:15:34 +0100

    LudoBase

---
+++ src/LudoGameApp/GameEngine/LudoEngine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameEngine
+{
+    public class LudoEngine
+    {
+        public void StartGame(int numberOfPlayers)
+        {
+            Console.WriteLine($"Game started with {numberOfPlayers} players");
+        }
+    }
+}
---
+++ src/LudoGameApp/LudoGameApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using GameEngine;
+using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Ludo!");
+            Console.WriteLine("How many players:");
+            int numberOfPlayers = int.Parse(Console.ReadLine());
+            var game = new LudoEngine();
+            game.StartGame(numberOfPlayers);
+
+        }
+    }
+}
